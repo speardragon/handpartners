55502a53e0742b8028b0350645b61d1161479cdd
speardragon <rkdckdfyyd@naver.com>
2024-08-25 20:51:39 +0900

    chore: set Envrionment

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="flex w-full h-full justify-center items-center bg-blue-50">
+      <div className="flex flex-col">
+        <div>Login</div>
+        <div>Image</div>
+      </div>
+    </div>
+  );
+}
