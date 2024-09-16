0b137f89ae5e0bc007a8acd9743c370030bc050a
speardragon <rkdckdfyyd@naver.com>
2024-09-16 23:51:10 +0900

    feat: add login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { getUsers } from "src/actions/user-actions";
+
 export default function Page() {
+  const userQuery = useQuery({
+    queryKey: ["users"],
+    queryFn: () => getUsers(),
+  });
   return (
     <div className="flex w-full h-full justify-center items-center bg-blue-50">
       <div className="flex flex-col">
         <div>Login</div>
-        <div>Image</div>
+        <div>동의</div>
+        <div>username</div>
+        <div>password</div>
+        {/* {userQuery.data && userQuery.data.map((user) => <div>{user.name}</div>)} */}
       </div>
     </div>
   );
